1053a09a32707fd396057ced4bd7e93b80400018
erfan <erfandc.es@gmail.com>
2025-02-07 19:41:57 +0330

    add data in reldatabase in fire base in cal data for carosel

--- src/Component/Main/Carousel.jsx
+++ src/Component/Main/Carousel.jsx
@@ -1,141 +1,69 @@
-import { useState } from 'react'
 import Slider from 'react-slick'
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { getDataFromFirebase } from '../../pages/firebase';
 import '../../css/slider.css'
-
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 export default function Carousel() {
-  const [sliderRef, setSliderRef] = useState(null)
+  const [sliderRef, setSliderRef] = useState(null);
+  const [hotelCards, setHotelCards] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const sliderSettings = {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 1,
-    infinite: true,
+    infinite: true,   // حتماً فعال باشد
     autoplay: false,
-     autoplaySpeed: 2000,
-     pauseOnHover: true,
-     centerMode: true,      
-     centerPadding: "20px", 
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    centerMode: true,  // این مقدار باید فعال باشد
+    centerPadding: "10px", // مقدار کم ولی نه 0
     responsive: [
-      {
-        breakpoint: 2561,
-        settings: {
-          slidesToShow: 6,
-        },
-      },
-      {
-        breakpoint: 1441,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 430,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
+      { breakpoint: 2561, settings: { slidesToShow: 6 } },
+      { breakpoint: 1441, settings: { slidesToShow: 4 } },
+      { breakpoint: 1025, settings: { slidesToShow: 3 } },
+      { breakpoint: 769, settings: { slidesToShow: 2 } },
+      { breakpoint: 430, settings: { slidesToShow: 1 } },
     ],
+  };
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getDataFromFirebase();
+        setHotelCards(data)
+      } catch (error) {
+        console.error("خطا در دریافت داده‌ها از Firebase:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div className="loading">درحال بارگذاری ...</div>;
   }
 
-  const hotelCards = [
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
-      title: 'هتل مرمر کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      title: 'هل امیر کبیر کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1599619351208-3e6c839d6828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
-      title: 'هتل کندلوس کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه','ناهار'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-      title: 'هتل پردیس کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه','ناهار'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
-      title: 'هتل سامان کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      title: 'هتل مرمر کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1599619351208-3e6c839d6828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
-      title: 'هتل کندلوس کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه','ناهار'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-      title: 'هتل پردیس کرج',
-      description: 'امکانات هتل',
-      pricingText: '285,000تومان/ روزانه',
-      features: ['وای فای ', 'صبحانه','ناهار'],
-    },
-  ]
-
   return (
-    <div className='C-1 container-fluid'>
-
+    <div className="C-1 container-fluid">
       <Slider ref={setSliderRef} {...sliderSettings}>
         {hotelCards.map((card, index) => (
-          <div key={index} className='card Sliderstyle' >
-            <img src={card.imageSrc} alt={card.title} className='card-image' />
-            <div className='text-infoo' dir='rtl'>
-              <div className='card-header c_header'>
-                <h2 >{card.title}</h2>
-                <span className='text-success'>{card.pricingText}</span>
+          <div key={index} className="card Sliderstyle">
+            <img src={card.imageSrc} alt={card.title} className="card-image" />
+            <div className="text-infoo" dir="rtl">
+              <div className="card-header c_header">
+                <h2>{card.title}</h2>
+                <span className="text-success">{card.pricingText}</span>
               </div>
-              <p className=''>{card.description}</p>
-              <ul className='items'>
-                {card.features.map((feature, index) => (
-                  <li key={index}>{feature}</li>
+              <p>{card.description}</p>
+              <ul className="items">
+                {card.features.map((feature, idx) => (
+                  <li key={idx}>{feature}</li>
                 ))}
               </ul>
             </div>
@@ -143,14 +71,14 @@ export default function Carousel() {
           </div>
         ))}
       </Slider>
-      <div className='C-2 controls d-flex justify-content-center '>
-        <button onClick={sliderRef?.slickPrev}>
+      <div className="C-2 controls d-flex justify-content-center">
+        <button onClick={() => sliderRef?.slickPrev()}>
           <FaChevronLeft />
         </button>
-        <button onClick={sliderRef?.slickNext}>
+        <button onClick={() => sliderRef?.slickNext()}>
           <FaChevronRight />
         </button>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app' ;
 import {getAuth} from 'firebase/auth'; 
-
+import {getDatabase , ref , get} from 'firebase/database';
 const firebaseConfig = {
     apiKey: "AIzaSyBvcDGRYuqyh3mRQ7Yb5z6kz8fd8LqEgWM",
     authDomain: "gardeshgary-a752b.firebaseapp.com",
@@ -10,5 +10,26 @@ const firebaseConfig = {
     appId: "1:580113953192:web:3244092d7f77098647ed9d",
 }
 const app = initializeApp(firebaseConfig);
+
+const getDataFromFirebase = async () =>{
+    const db = getDatabase();
+    const dataRef = ref(db , 'hotel');
+    
+    try{
+        const snapshot = await get(dataRef);
+        if(snapshot.exists()){
+            console.log(snapshot.val());
+            return snapshot.val();
+        }else{
+            console.log('no data available');
+            return null;
+        }
+    }catch(error){
+        console.error("Error reading data from Firebase:", error);
+    }
+}
+
 export const auth = getAuth(app); 
-export default app; 
+export default app;
+export {getDataFromFirebase};
+ 
